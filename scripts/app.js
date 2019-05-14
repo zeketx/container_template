25ade7c967e36fc1ae7cbdc77db3db181a4ebe36
zeketx <zekemauricio@icloud.com>
2019-05-14 00:18:49 -0500

    new hero images and UI, Hero 2 slider

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// navbar toggle IIFE
+
 (function navbarToggle() {
     const navToggler = document.querySelector(".nav_toggler");
     const navMenu = document.querySelector(".navbar ul");
@@ -19,3 +21,34 @@
     }
   })();
   
+// animate.css 
+
+  const animateNavSvg = document.querySelector('.nav_svg');
+  animateNavSvg.classList.add('animated', 'fadeInDown', 'slow');
+
+  const animateBtn = document.querySelector('.herobtn');
+  animateBtn.classList.add('animated', 'infinite', 'bounce', 'slower');
+
+  const animatePara = document.querySelector('.about_para');
+  animatePara.classList.add('wow', 'animated', 'fadeInRight', 'slow');
+
+  const animateHero_2 = document.querySelector('.hero_2');
+  animateHero_2.classList.add('wow', 'animated', 'fadeIn', 'slower');
+
+
+// hero 2 Slider 
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
